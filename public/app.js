f099327b3b3ed59567e72cf076ce6f8812dc188d
Sevka <sevkaplay@gmail.com>
2020-03-29 21:02:34 +0300

    Add auto-refresh after adding new course in cart

--- public/app.js
+++ public/app.js
@@ -1,25 +1,51 @@
-document.querySelectorAll(".price").forEach(node => {
-    node.textContent = new Intl.NumberFormat("ru-RU", {
+const toCurrency = price => {
+    return new Intl.NumberFormat("ru-RU", {
         currency: "rub",
         style: "currency"
-    }).format(node.textContent);
+    }).format(price);
+};
+
+document.querySelectorAll("#price").forEach(node => {
+    node.textContent = toCurrency(node.textContent);
 });
 
-const $cart = document.querySelector('#cart')
+const $cart = document.querySelector("#cart");
 if ($cart) {
-  $cart.addEventListener('click', event => {
-    if (event.target.classList.contains('remove')) {
-      const id = event.target.dataset.id
-      fetch('/cart/remove/' + id, {
-        method: 'delete'
-      })
-      .then(res => res.json())
-      .then(cart => {
-        console.log(cart);
-      })
+    $cart.addEventListener("click", event => {
+        if (event.target.classList.contains("remove")) {
+            const id = event.target.dataset.id;
+            fetch("/cart/remove/" + id, {
+                method: "delete"
+            })
+                .then(res => res.json())
+                .then(cart => {
+                    if (cart.courses.length) {
+                        const HTML = cart.courses
+                            .map(c => {
+                                return `
+            <tbody>
+              <tr>
+                  <th>${c.title}</th>
+                  <th>${c.count}</th>
+                  <th>${c.price}</th>
+                  <th>
+                      <button class="btn btn-danger remove" data-id="${c.id}">Удалить</button>
+                  </th>
+              </tr>
+            </tbody>
+            `;
+                            })
+                            .join("");
 
-    }
-  })
+                        document.querySelector("tbody").innerHTML = HTML;
+                        document.querySelector(
+                            "#price"
+                        ).innerText = `${toCurrency(cart.price)}`;
+                    } else {
+                        $cart.innerHTML = "<p>Корзина пуста</p>";
+                    }
+                });
+        }
+    });
 } else {
-
 }
