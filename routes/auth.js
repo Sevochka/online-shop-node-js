f1230cc6966028573e8c8ae88aa7b9b196c0b475
Sevochka <sevkaplay@gmail.com>
2020-04-07 18:17:50 +0300

    SignIn works with database

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
-const { Router } = require("express");
+const {
+    Router
+} = require("express");
 const router = Router();
 const User = require("../models/user");
 
@@ -17,40 +19,68 @@ router.get("/logout", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-    const user = await User.findById("5e8336162ef2323f04a798ce");
-    //Сохраняем пользователя в обхекты сесиии, чтобы потом хватать
-    req.session.user = user;
-    req.session.isAuthenticated = true;
-    //Для того, чтобы редирект произошел после установления сессии
-    req.session.save((err) => {
-        if (err) throw err;
-        else res.redirect("/");
-    });
+    try {
+        const {
+            email,
+            password
+        } = req.body;
+
+        const candidate = await User.findOne({
+            email
+        })
+        if (candidate) {
+            const areSame = password === candidate.password;
+
+            if (areSame) {
+                //Сохраняем пользователя в обхекты сесиии, чтобы потом хватать
+                req.session.user = candidate;
+                req.session.isAuthenticated = true;
+                //Для того, чтобы редирект произошел после установления сессии
+                req.session.save((err) => {
+                    if (err) throw err;
+                    else res.redirect("/");
+                });
+            } else {
+                res.redirect('/auth/login#login')
+            }
+        } else {
+            res.redirect('/auth/login#login')
+        }
+    } catch (error) {
+        throw error
+    }
 });
 
 router.post("/register", async (req, res) => {
-   try {
-      const { email, password, repeat, name } = req.body;
+    try {
+        const {
+            email,
+            password,
+            repeat,
+            name
+        } = req.body;
 
-      const isExist = await User.findOne({ email })
+        const isExist = await User.findOne({
+            email
+        })
 
-      if (isExist) {
-          return res.redirect('/auth/login#register')
-      }
+        if (isExist) {
+            return res.redirect('/auth/login#register')
+        }
 
-      const user = new User({
-          email,
-          name,
-          password,
-          cart: {
-              items:[]
-          }
-      })
-      await user.save();
-      res.redirect('/auth/login#login')
-   } catch (error) {
-       throw error
-   }
+        const user = new User({
+            email,
+            name,
+            password,
+            cart: {
+                items: []
+            }
+        })
+        await user.save();
+        res.redirect('/auth/login#login')
+    } catch (error) {
+        throw error
+    }
 });
 
 module.exports = router;
