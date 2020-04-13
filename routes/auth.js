25042677cde825588f1489143602c79aa0ca5ec0
Sevka <sevkaplay@gmail.com>
2020-04-13 11:57:41 +0300

    Add utils validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,28 @@
 const { Router } = require("express");
 const router = Router();
 const User = require("../models/user");
-const encryptor = require('bcryptjs');
-const nodemailer = require('nodemailer');
-const sendgrid = require('nodemailer-sendgrid-transport');
-const keys = require('../keys')
-const regEmail = require("../emails/registration")
-const crypto = require('crypto');
+const encryptor = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgrid = require("nodemailer-sendgrid-transport");
+const keys = require("../keys");
+const regEmail = require("../emails/registration");
+const crypto = require("crypto");
 const resetEmail = require("../emails/reset");
 //validation
-const { validationResult } = require('express-validator')
-const { registerValidators } = require("../utils/validators")
+const { validationResult } = require("express-validator");
+const { registerValidators } = require("../utils/validators");
 
-const transporter = nodemailer.createTransport(sendgrid({
-    auth:{api_key: keys.SENDGRID_API_KEY}
-}))
+const transporter = nodemailer.createTransport(
+    sendgrid({
+        auth: { api_key: keys.SENDGRID_API_KEY },
+    })
+);
 
 router.get("/login", async (req, res) => {
     res.render("auth/login", {
         title: "Авторизация",
         isLogin: true,
-        loginError: req.flash('loginError')
+        loginError: req.flash("loginError"),
     });
 });
 
@@ -28,7 +30,7 @@ router.get("/register", async (req, res) => {
     res.render("auth/register", {
         title: "Регистрация",
         isRegister: true,
-        registerError: req.flash('registerError')
+        registerError: req.flash("registerError"),
     });
 });
 
@@ -41,17 +43,17 @@ router.get("/logout", async (req, res) => {
 
 router.post("/login", async (req, res) => {
     try {
-        const {
-            email,
-            password
-        } = req.body;
+        const { email, password } = req.body;
 
         const candidate = await User.findOne({
-            email
-        })
+            email,
+        });
         if (candidate) {
-            //Сравниваем пароль из БД с введенным 
-            const areSame = await encryptor.compare(password, candidate.password)
+            //Сравниваем пароль из БД с введенным
+            const areSame = await encryptor.compare(
+                password,
+                candidate.password
+            );
 
             if (areSame) {
                 //Сохраняем пользователя в объект сесиии, чтобы потом хватать
@@ -65,82 +67,68 @@ router.post("/login", async (req, res) => {
             } else {
                 //Если пароли из БД и введенный не свопадают
                 //Если бы в детстве тебя почаще пороли, тебе бы подходили пароли, ну а так ты не подходишь по роли
-                req.flash('loginError', 'Неправильный пароль')
-                res.redirect('/auth/login#login')
+                req.flash("loginError", "Неправильный пароль");
+                res.redirect("/auth/login#login");
             }
         } else {
             //Если маил не свопадает
-            req.flash('loginError', 'Такого пользователя не существует')
-            res.redirect('/auth/login')
+            req.flash("loginError", "Такого пользователя не существует");
+            res.redirect("/auth/login");
         }
     } catch (error) {
-        throw error
+        throw error;
     }
 });
 
 router.post("/register", registerValidators, async (req, res) => {
     try {
-        const {
-            email,
-            password,
-            confirm,
-            name
-        } = req.body;
-
-        const isExist = await User.findOne({
-            email
-        })
+        const { email, password, name } = req.body;
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            req.flash('registerError', errors.array()[0].msg)
-            return res.status(422).redirect('/auth/register')
+            req.flash("registerError", errors.array()[0].msg);
+            return res.status(422).redirect("/auth/register");
         }
 
-        if (isExist) {
-            req.flash('registerError', 'Такой email уже занят')
-            return res.redirect('/auth/login')
-        } else {
-            //Шифратор паролей, второй параметр - точность шифратора - чем больше, тем круче шифрование
-            //но дольше времени занимает
-            const hashPassword = await encryptor.hash(password, 10);
-            console.log(hashPassword);
-            
-            const user = new User({
-                email,
-                name,
-                password: hashPassword,
-                cart: {
-                    items: []
-                }
-            })
-            await user.save();
-            //Сервис по отпарвке emails
-            //await transporter.sendMail(regEmail(email))
-            res.redirect('/auth/login');
-        }
+        //Шифратор паролей, второй параметр - точность шифратора - чем больше, тем круче шифрование
+        //но дольше времени занимает
+        const hashPassword = await encryptor.hash(password, 10);
+        console.log(hashPassword);
+
+        const user = new User({
+            email,
+            name,
+            password: hashPassword,
+            cart: {
+                items: [],
+            },
+        });
+        await user.save();
+        //Сервис по отпарвке emails
+        //await transporter.sendMail(regEmail(email))
+        res.redirect("/auth/login");
     } catch (error) {
-        throw error
+        throw error;
     }
 });
 
-router.get('/reset', (req, res) => {
-    res.render('auth/reset', {
-        title:'Забыли пароль?', 
-        resetError: req.flash('resetError')
-    })
-})
+router.get("/reset", (req, res) => {
+    res.render("auth/reset", {
+        title: "Забыли пароль?",
+        resetError: req.flash("resetError"),
+    });
+});
 
-router.post('/reset', (req, res) => {
+router.post("/reset", (req, res) => {
     try {
         crypto.randomBytes(32, async (err, buffer) => {
             if (err) {
-                req.flash('error', "Что-то пошло не так, обидно!")
-                return res.redirect('/auth/reset')
+                req.flash("error", "Что-то пошло не так, обидно!");
+                return res.redirect("/auth/reset");
             }
 
-            const token = buffer.toString('hex');
-            const isExist = await User.findOne({email: req.body.email})
+            const token = buffer.toString("hex");
+            const isExist = await User.findOne({ email: req.body.email });
 
             if (isExist) {
                 isExist.resetToken = token;
@@ -148,65 +136,64 @@ router.post('/reset', (req, res) => {
                 isExist.resetTokenExp = Date.now() + 60 * 60 * 1000;
                 await isExist.save();
                 await transporter.sendMail(resetEmail(isExist.email, token));
-                res.redirect('/auth/login');
+                res.redirect("/auth/login");
             } else {
-                req.flash('resetError', "Такого емаил нет")
-                res.redirect('/auth/reset')
+                req.flash("resetError", "Такого емаил нет");
+                res.redirect("/auth/reset");
             }
-        })
+        });
     } catch (error) {
-        throw error
+        throw error;
     }
-})
+});
 
 router.get("/password/:token", async (req, res) => {
     if (!req.params.token) {
-        return res.redirect('/auth/login')
+        return res.redirect("/auth/login");
     }
 
     try {
         const user = await User.findOne({
             resetToken: req.params.token,
-            resetTokenExp: {$gt: Date.now()}
-        }) 
+            resetTokenExp: { $gt: Date.now() },
+        });
 
         if (!user) {
-            return res.redirect('/auth/login');
+            return res.redirect("/auth/login");
         } else {
             res.render("auth/password", {
                 title: "Восстановить доступ",
-                registerError: req.flash('error'),
+                registerError: req.flash("error"),
                 userId: user._id.toString(),
-                token: req.params.token
+                token: req.params.token,
             });
         }
     } catch (error) {
-        throw error
+        throw error;
     }
-    
 });
 
-router.post('/password', async (req, res) => {
+router.post("/password", async (req, res) => {
     try {
         const user = await User.findOne({
             _id: req.body.userId,
             resetToken: req.body.token,
-            resetTokenExp: {$gt: Date.now()}
-        })
+            resetTokenExp: { $gt: Date.now() },
+        });
 
         if (user) {
             user.password = await encryptor.hash(req.body.password, 10);
             user.resetToken = undefined;
-            user.resetTokenExp =  undefined;
-            await user.save()
-            res.redirect('/auth/login')
+            user.resetTokenExp = undefined;
+            await user.save();
+            res.redirect("/auth/login");
         } else {
-            req.flash('loginError', "Врямя жизни токеа истекло")
-            res.redirect('/auth/login')
+            req.flash("loginError", "Врямя жизни токеа истекло");
+            res.redirect("/auth/login");
         }
     } catch (e) {
-        throw e 
+        throw e;
     }
-})
+});
 
 module.exports = router;
