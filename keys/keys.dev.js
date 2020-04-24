df2a33a5cc481fd13de050bf5c43baa698ea8e67
Sevka <sevkaplay@gmail.com>
2020-04-24 22:56:08 +0300

    Atempt to fix

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,12 +1,4 @@
 const password = "ArNCYAJJdUkzLVMo";
-/**
- * Эскпортируемый объект со всеми основными константами для более удобного управления проектом.
- * @property {string} MONGO_URI - URI для установления соединения с БД.
- * @property {string} SESSION_SECRET - Секретный ключ для хеширования сеанса.
- * @property {string} SENDGRID_API_KEY - API ключ для сервиса отправки электронной почты.
- * @property {string} EMAIL_FROM - Указание email адреса, с которого осуществляется отправление писем.
- * @property {string} BASE_URL - Адрес сайта, необходимо изменить при продакшене.
- */
 
 module.exports = {
     MONGO_URI:`mongodb+srv://sevka:${password}@cluster0-e6cu6.mongodb.net/shop`,
