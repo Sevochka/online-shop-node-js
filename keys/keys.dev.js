791c08a142ae4013caefc4b64cf65404532e37be
Sevka <sevkaplay@gmail.com>
2020-04-24 23:09:08 +0300

    UI changes for heroku

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,7 +1,7 @@
 const password = "ArNCYAJJdUkzLVMo";
 
 module.exports = {
-    MONGO_URI:`mongodb+srv://sevka:${password}@cluster0-e6cu6.mongodb.net/shop`,
+    MONGO_URI:`mongodb+srv://sevka:ArNCYAJJdUkzLVMo@cluster0-e6cu6.mongodb.net/shop`,
     SESSION_SECRET: 'hello, world',
     SENDGRID_API_KEY: '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',
     EMAIL_FROM: "sevkaplay@gmail.com",
--- routes/home.js
+++ routes/home.js
@@ -4,10 +4,7 @@ const router = Router();
  * Отображение главной страницы
  */
 router.get("/", (req, res) => {
-    res.render("index", {
-        title: "Главная страница",
-        isHome:true
-    });
+    res.redirect('/courses')
 });
 
 module.exports = router;
